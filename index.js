7fb89fae2bd70e956f4ce7d6493b29da34c9d648
Zel <cmorriswrk@gmail.com>
2022-02-27 18:50:42 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require('path');
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.get('/about', function(req, res) {
+  res.sendFile(path.join(__dirname, '/about.html'));
+});
+
+app.get('/contactme', function(req, res) {
+    res.sendFile(path.join(__dirname, '/contact-me.html'));
+  });
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/404.html'));
+  });
+
+app.listen(port, () => {
+  console.log(`Listening on post ${port}`);
+});
